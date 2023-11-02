c2c41010a4d764017b4510e1b47dbb56f55bb71d
stupig <1727950162@qq.com>
2023-11-02 09:45:34 +0800

    update lab2

---
+++ lab2/dataset_divide.py
@@ -0,0 +1,62 @@
+# 对数据集进行划分
+import os
+import shutil
+import math
+import random
+
+# 数据集路径
+data_path = './lab2/data/Caltech101/caltech-101/101_ObjectCategories'
+output_path = './lab2/data/Caltech101'
+
+# 创建训练集、验证集和测试集的文件夹
+train_path = os.path.join(output_path, 'train')
+val_path = os.path.join(output_path, 'val')
+test_path = os.path.join(output_path, 'test')
+
+os.makedirs(train_path, exist_ok=True)
+os.makedirs(val_path, exist_ok=True)
+os.makedirs(test_path, exist_ok=True)
+
+# 定义划分比例
+train_ratio = 0.8
+val_ratio = 0.1
+test_ratio = 0.1
+
+# 划分函数
+def split_data(class_dir):
+    images = os.listdir(class_dir)
+    images.sort()  # 按数字升序排列图片
+
+    n = len(images)
+    train_count = math.floor(train_ratio * n)
+    val_count = math.floor((train_ratio + val_ratio) * n)
+
+    train_images = images[:train_count]
+    val_images = images[train_count:val_count]
+    test_images = images[val_count:]
+
+    return train_images, val_images, test_images
+
+# 遍历数据集中的每个类别文件夹
+for category in os.listdir(data_path):
+    category_path = os.path.join(data_path, category)
+
+    train_category_path = os.path.join(train_path, category)
+    val_category_path = os.path.join(val_path, category)
+    test_category_path = os.path.join(test_path, category)
+
+    os.makedirs(train_category_path, exist_ok=True)
+    os.makedirs(val_category_path, exist_ok=True)
+    os.makedirs(test_category_path, exist_ok=True)
+
+    train_images, val_images, test_images = split_data(category_path)
+
+    # 将图片分配到训练集、验证集和测试集的相应文件夹中
+    for img in train_images:
+        shutil.copy(os.path.join(category_path, img), os.path.join(train_category_path, img))
+
+    for img in val_images:
+        shutil.copy(os.path.join(category_path, img), os.path.join(val_category_path, img))
+
+    for img in test_images:
+        shutil.copy(os.path.join(category_path, img), os.path.join(test_category_path, img))
---
+++ lab2/train.py
@@ -0,0 +1,108 @@
+# 训练代码
+import torch
+import os
+from torchvision import datasets, models, transforms
+import torch.optim as optim
+import torch.nn as nn
+from model import AlexNet
+from datetime import datetime   
+from torch.utils.tensorboard import SummaryWriter
+
+def evaluate(model, dataloader, device, data_name, epoch):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1) # 第一个返回值为最大值，第二个为最大值索引
+            total +=labels.size(0)
+            correct += (predicted == labels).sum().item()
+    accuracy = 100*correct/total
+    print(f"Accuracy on the {data_name}: {accuracy :.2f}%")
+    writer.add_scalar(f'Accuracy/{data_name}', accuracy, epoch)
+
+# dataset 路径
+data_dir = './lab2/data/Caltech101'
+# log_dir = './lab2/log'
+log_dir = "B:/log"
+
+# 格式化当前时间
+current_time = datetime.now()
+folder_name = current_time.strftime("%m-%d-%H-%M")
+
+tensorboard_dir = os.path.join(log_dir, folder_name)
+os.makedirs(tensorboard_dir, exist_ok=True)
+writer = SummaryWriter(tensorboard_dir)
+
+# 网络参数
+batchsize = 16
+lr = 0.0001
+momentum = 0.9
+epochs = 500
+validation_frequency = 10
+
+# resize图片，变为224*224大小；对训练集进行数据增强；将图像转为tensor
+# transforms提供了各种图像数据预处理方法
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.Resize((224, 224)),  # 调整图像大小
+        # transforms.RandomHorizontalFlip(), # 随机翻转
+        transforms.ToTensor(),
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize((224, 224)),  
+        transforms.ToTensor(),
+    ]),
+    'test': transforms.Compose([
+        transforms.Resize((224, 224)), 
+        transforms.ToTensor(),
+    ])
+}
+
+
+# 创建dataset和dataloader
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val', 'test']} # 以for循环的方式创建了一个字典image_dataset，字典中有三个键“train”，“val”，“test”，键值为dataset.ImageFolder
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batchsize, shuffle=True) for x in ['train', 'val', 'test']} # 为三个集创建dataloader
+
+# 创建模型
+model = AlexNet()
+
+# 设置训练设备 
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("successfully use gpu")
+else:
+    print("pay attention, use cpu now")
+
+model = model.to(device)
+
+# 定义损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum) # 随机梯度下降
+
+# 训练
+print("start train")
+for epoch in range(epochs):
+    model.train()
+    epoch_loss = 0.0
+    for inputs, labels in dataloaders['train']: # inputs:(16,3,224,224) labels:(16)
+        # print(inputs.shape, labels.shape)
+        inputs, labels = inputs.to(device), labels.to(device) # 移入GPU
+        optimizer.zero_grad() # 梯度归零
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward() 
+        optimizer.step() # 更新参数
+        epoch_loss += loss.item() * inputs.size(0)
+
+    avg_loss = epoch_loss/len(image_datasets['train'])
+    writer.add_scalar('Loss/train', avg_loss, epoch)
+    print(f"Epoch: {epoch+1}   loss: {avg_loss}")
+
+    if (epoch+1)%validation_frequency == 0:
+        evaluate(model, dataloaders['train'], device, 'train', epoch)
+        evaluate(model, dataloaders['val'], device, 'validation', epoch)
+
+evaluate(model, dataloaders['test'], device, 'test', epoch)
